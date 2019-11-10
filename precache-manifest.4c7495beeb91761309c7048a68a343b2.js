c1ee24757816254e1aec4967bb7eb143a81e6c50
ifjso <ojsboy@hotmail.com>
2019-11-11 02:15:50 +0900

    Updates

---
+++ precache-manifest.4c7495beeb91761309c7048a68a343b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57527a587c3e79a67288077edccc91e1",
+    "url": "/gift-for-you/index.html"
+  },
+  {
+    "revision": "d786bb9d0f7a08497452",
+    "url": "/gift-for-you/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e9170e5946300db7ac89",
+    "url": "/gift-for-you/static/js/2.1eaf8296.chunk.js"
+  },
+  {
+    "revision": "d786bb9d0f7a08497452",
+    "url": "/gift-for-you/static/js/main.b5bc995d.chunk.js"
+  },
+  {
+    "revision": "9032ea164cdf9aa334fc",
+    "url": "/gift-for-you/static/js/runtime-main.edba1299.js"
+  }
+]);
