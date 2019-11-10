3501b61aa1e5357daf1fa9a4751d9d657f778146
ifjso <ojsboy@hotmail.com>
2019-11-10 21:26:29 +0900

    dark theme 적용

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import GiftSystem from './components/GiftSystem';
 
-function App() {
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#90caf9'
+    },
+    background: {
+      default: '#121212'
+    }
+  }
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <GiftSystem />
+    </MuiThemeProvider>
   );
-}
+};
 
 export default App;
